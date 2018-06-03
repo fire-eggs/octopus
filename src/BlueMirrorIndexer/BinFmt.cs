8a0dc45195c46ec6bf84ad131a4365e67a1486d8
fire-eggs <lifeattickville@gmail.com>
2018-06-03 14:27:23 -0400

    1. Add header to BIN format. 2. Add logical folders support to BIN format.

--- src/BlueMirrorIndexer/BinFmt.cs
+++ src/BlueMirrorIndexer/BinFmt.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+
+// TODO seriously consider markers around parts (discs,folders,files,lfolds)
 
 namespace BlueMirrorIndexer
 {
     public class BinFmt
     {
         private int tick;
-        private void logit(string msg, bool first = false)
+
+        public void logit(string msg, bool first = false)
         {
             int delta = 0;
             if (first)
@@ -32,6 +37,8 @@ namespace BlueMirrorIndexer
             using (var f = File.Open(binFile, FileMode.Open))
             using (var r = new BinaryReader(f))
             {
+                if (!ReadHeader(r))
+                    return null; // Not a compatible octopus file
                 mem = ReadData(r);
             }
             bf.logit("ReadFromBin-Done");
@@ -51,6 +58,9 @@ namespace BlueMirrorIndexer
             using (var f = File.Open(binFile, FileMode.Create))
             using (var w = new BinaryWriter(f))
             {
+                WriteHeader(w);
+                // Logical folders have reference to data items using DbId
+                WriteLogicalFolders(w, mem);
                 WriteData(w, mem);
             }
 
@@ -58,10 +68,19 @@ namespace BlueMirrorIndexer
         }
 
         #region Write Data
-        public static void WriteData(BinaryWriter w, VolumeDatabase mem)
-        {
-            // TODO version header
 
+        private static uint _databaseId;
+
+        private static void WriteHeader(BinaryWriter f)
+        {
+            // TODO fetch version # from someplace
+            f.Write("|Octopus_3.0.0.1|");
+
+            _databaseId = 0; // Fresh assignment of ids
+        }
+
+        private static void WriteData(BinaryWriter w, VolumeDatabase mem)
+        {
             var discList = mem.GetDiscs();
             int totDiscs = discList.Count;
             w.Write(totDiscs);
@@ -69,13 +88,18 @@ namespace BlueMirrorIndexer
             {
                 var disc = discList[discCount];
                 WriteDisk(w, disc);
-                WriteFiles(w, -discCount, ((IFolder)disc).Files);
-                WriteFolders(w, -discCount, ((IFolder)disc).Folders);
+                WriteFiles(w, ((IFolder)disc).Files);
+                WriteFolders(w, ((IFolder)disc).Folders);
             }
         }
 
         private static void WriteDisk(BinaryWriter w, DiscInDatabase disc)
         {
+            w.Write("|Disk|");
+
+            //disc.DbId = ++_databaseId;
+            w.Write(disc.DbId);
+
             w.Write(disc.DriveFormat);
             w.Write((int) disc.DriveType);
             w.Write(disc.TotalFreeSpace);
@@ -92,14 +116,16 @@ namespace BlueMirrorIndexer
             w.Write(disc.ClusterSize);
         }
 
-        private static void WriteFiles(BinaryWriter w, int owner, FileInDatabase[] files)
+        private static void WriteFiles(BinaryWriter w, FileInDatabase[] files)
         {
             int totFiles = files.Length;
             w.Write(totFiles);
             for (int fileDex = 0; fileDex < totFiles; fileDex++)
             {
                 var afile = files[fileDex];
-                w.Write(owner);
+
+                //afile.DbId = ++_databaseId;
+                w.Write(afile.DbId);
 
                 w.Write(afile.Name);
                 w.Write(afile.Extension);
@@ -113,10 +139,9 @@ namespace BlueMirrorIndexer
                 w.Write(afile.Description);
                 w.Write(afile.CRC);
             }
-            
         }
 
-        private static void WriteFolders(BinaryWriter w, int owner, IFolder[] folds)
+        private static void WriteFolders(BinaryWriter w, IFolder[] folds)
         {
             int totFolds = folds.Length;
             w.Write(totFolds);
@@ -127,7 +152,8 @@ namespace BlueMirrorIndexer
                 if (afile == null)
                     continue; // TODO BUG file count is now wrong!
 
-                w.Write(owner);
+                //afile.DbId = ++_databaseId;
+                w.Write(afile.DbId);
 
                 w.Write(afile.Name);
                 w.Write(afile.Extension);
@@ -139,8 +165,41 @@ namespace BlueMirrorIndexer
                 w.Write(afile.Keywords);
                 w.Write(afile.Description);
 
-                WriteFiles(w, foldDex, afold.Files);
-                WriteFolders(w, foldDex, afold.Folders);
+                WriteFiles(w, afold.Files);
+                WriteFolders(w, afold.Folders);
+            }
+        }
+
+        private static void WriteLogicalFolders(BinaryWriter w, VolumeDatabase mem)
+        {
+            w.Write("|LFold|");
+            var lfolds = mem.GetLogicalFolders();
+            w.Write(lfolds.Count);
+            foreach (var lfold in lfolds)
+            {
+                WriteLogicalFolder(w, lfold);
+            }
+        }
+
+        private static void WriteLogicalFolder(BinaryWriter w, LogicalFolder f)
+        {
+            w.Write(f.Name);
+            w.Write(f.Description);
+            w.Write((int)f.FolderType);
+
+            w.Write(f.Items.Length);
+            foreach (var iid in f.Items)
+            {
+                // Only those items referenced by logical folder will get an id
+                if (iid.DbId == 0)
+                    iid.DbId = ++_databaseId;
+                w.Write(iid.DbId);
+            }
+
+            w.Write(f.GetSubFolders().Count);
+            foreach (var subFolder in f.GetSubFolders())
+            {
+                WriteLogicalFolder(w, subFolder);
             }
         }
 
@@ -148,14 +207,25 @@ namespace BlueMirrorIndexer
 
         #region Read Data
 
+        private static bool ReadHeader(BinaryReader r)
+        {
+            var head = r.ReadString();
+            return head == "|Octopus_3.0.0.1|";
+
+            // TODO version handling
+        }
+
         private static VolumeDatabase ReadData(BinaryReader r)
         {
             VolumeDatabase mem = new VolumeDatabase();
 
+            // Logical folders first: they have reference to DbIds ahead
+            ReadLFolds(r, mem);
+            
             int discCount = r.ReadInt32();
-            for (int dex = 0; dex < discCount; dex++)
+            for (uint dex = 0; dex < discCount; dex++)
             {
-                var did = ReadDisc(r, dex);
+                var did = ReadDisc(r);
                 mem.AddDisc(did);
                 ReadFiles(r, did);
                 ReadFolds(r, did);
@@ -164,9 +234,14 @@ namespace BlueMirrorIndexer
             return mem;
         }
 
-        private static DiscInDatabase ReadDisc(BinaryReader r, int dbId)
+        private static DiscInDatabase ReadDisc(BinaryReader r)
         {
+            var head = r.ReadString();
+
+            var dbId = r.ReadUInt32();
             DiscInDatabase did = new DiscInDatabase(dbId);
+
+            HookLogicalFolder(dbId, did);
 
             did.DriveFormat = r.ReadString();
             did.DriveType= (DriveType)r.ReadInt32();
@@ -190,9 +265,9 @@ namespace BlueMirrorIndexer
         private static void ReadFiles(BinaryReader r, IFolder did)
         {
             int totFiles = r.ReadInt32();
-            for (int dex = 0; dex < totFiles; dex++)
+            for (uint dex = 0; dex < totFiles; dex++)
             {
-                r.ReadInt32(); // TODO skip owner?
+                uint dbId = r.ReadUInt32();
 
                 string name = r.ReadString();
                 string ext = r.ReadString();
@@ -206,23 +281,27 @@ namespace BlueMirrorIndexer
                 string desc = r.ReadString();
                 uint crc = r.ReadUInt32();
 
-                FileInDatabase afile = new FileInDatabase(dex, name, ext, fname, fa, len, keyw);
+                FileInDatabase afile = new FileInDatabase(dbId, name, ext, fname, fa, len, keyw);
                 afile.SetTimes(cTicks, aTicks, wTicks);
                 afile.Description = desc;
                 afile.CRC = crc;
 
                 did.AddToFiles(afile);
+
+                HookLogicalFolder(dbId, afile);
             }
         }
 
         private static void ReadFolds(BinaryReader r, IFolder did)
         {
             int totFolds = r.ReadInt32();
-            for (int dex = 0; dex < totFolds; dex++)
+            for (uint dex = 0; dex < totFolds; dex++)
             {
+                uint dbId = r.ReadUInt32();
+
                 // TODO COMPRESSED_FLAG hack: should write attributes first
-                ItemInDatabase afile = new FolderInDatabase(dex, did);
-                r.ReadInt32(); // TODO skip owner?
+                ItemInDatabase afile = new FolderInDatabase(dbId, did);
+
                 afile.Name = r.ReadString();
                 afile.Extension = r.ReadString();
                 afile.FullName = r.ReadString();
@@ -238,9 +317,78 @@ namespace BlueMirrorIndexer
 
                 did.AddToFolders(afile as IFolder);
 
+                HookLogicalFolder(dbId, afile);
+
                 ReadFiles(r, afile as IFolder);
                 ReadFolds(r, afile as IFolder);
             }
+        }
+
+        // TODO dictionary is overkill? array, index on dbid sufficient?
+        private static Dictionary<uint, List<LogicalFolder>> _lfoldMap;
+
+        private static void AddToLFoldMap(uint dbid, LogicalFolder refer)
+        {
+            List<LogicalFolder> lfolds;
+            if (_lfoldMap.TryGetValue(dbid, out lfolds))
+            {
+                lfolds.Add(refer);
+            }
+            else
+            {
+                lfolds = new List<LogicalFolder>();
+                lfolds.Add(refer);
+                _lfoldMap.Add(dbid, lfolds);
+            }
+        }
+
+        // Determine if an entity needs to be connected to a logical folder
+        private static void HookLogicalFolder(uint dbid, ItemInDatabase iid)
+        {
+            if (dbid == 0 || iid == null)
+                return;
+            var lfolds = _lfoldMap[dbid];
+            foreach (var logicalFolder in lfolds)
+            {
+                logicalFolder.AddItem(iid);
+            }
+        }
+
+        private static void ReadLFolds(BinaryReader r, VolumeDatabase mem)
+        {
+            var head = r.ReadString();
+
+            _lfoldMap = new Dictionary<uint, List<LogicalFolder>>();
+
+            int count = r.ReadInt32();
+            for (int i = 0; i < count; i++)
+            {
+                var lf = ReadLogFold(r);
+                mem.AddLogicalFolder(lf);
+            }
+        }
+
+        private static LogicalFolder ReadLogFold(BinaryReader r)
+        {
+            LogicalFolder lf = new LogicalFolder();
+            lf.Name = r.ReadString();
+            lf.Description = r.ReadString();
+            lf.FolderType = (LogicalFolderType)r.ReadInt32();
+
+            int itemCount = r.ReadInt32();
+            for (int j = 0; j < itemCount; j++)
+            {
+                var dbid = r.ReadUInt32(); // TODO find ItemInDatabase and connect
+                AddToLFoldMap(dbid, lf);
+            }
+
+            int subfolds = r.ReadInt32();
+            for (int j = 0; j < subfolds; j++)
+            {
+                var slf = ReadLogFold(r);
+                lf.GetSubFolders().Add(slf);
+            }
+            return lf;
         }
 
         #endregion
